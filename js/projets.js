396e5860d96a4a56b13d9a56f2a1e1d95987c6c8
mikagame <mikael.boichot@gmail.com>
2021-10-24 11:54:18 +0200

    création site

--- js/projets.js
+++ js/projets.js
@@ -3,6 +3,7 @@ function visible() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById('projets').classList.add('show');
         document.getElementById('projets').classList.replace('hide', 'show');
+        document.getElementById('T').classList.add('changeColor');
         document.getElementById('T').classList.replace('colorBorder', 'changeColor')
     } else {
         document.getElementById('projets').classList.replace('show', 'hide');
